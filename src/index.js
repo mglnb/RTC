ec7567cf2e077b07ad97f256f8a3e78f3c5ffe2f
Miguel Boanova <miguel.boanova@prodigious.com>
2018-01-11 17:05:49 -0200

    atualizado html

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import $ from 'jquery';
-import add from './sample';
-import './styles.scss';
-
-const app = $('#App');
-const sum = add(1, 2);
-app.html(`Hello World! 1 + 2 = ${sum}`);
+import './js/plugins/adapter'
+import './styles.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const path = require('path')
 const srcDir = path.join(__dirname, './src/')
 const dstDir = path.join(__dirname, './build/')
 
-// Okay, this may be confusing at first glance but go through it step-by-step
 module.exports = env => {
   const ifProd = plugin => (env.prod ? plugin : undefined)
   const isDev = plugin => (env.dev ? plugin : undefined)
@@ -19,27 +18,18 @@ module.exports = env => {
   })
 
   return {
-    /**
-     * entry tells webpack where to start looking.
-     */
     entry: {
       app: srcDir
     },
-    /**
-     * output tells webpack where to put the files he creates
-     * after running all its loaders and plugins.
-     */
     output: {
       filename: '[name].js',
       path: dstDir
     },
-
     module: {
-      // Loaders allow you to preprocess files!
       loaders: [{
-        test: /\.(js)$/, // look for .js files
-        exclude: /node_modules/, // ingore /node_modules
-        loader: 'babel-loader' // preprocess with that babel goodness
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       },
       {
         test: /\.s?css$/,
@@ -62,28 +52,20 @@ module.exports = env => {
         })
       }]
     },
-
     plugins: removeEmpty([
-      /**
-       * HtmlWebpackPlugin will make sure out JavaScriot files are being called
-       * from within our index.html
-       */
       new HtmlWebpackPlugin({
         template: path.join(__dirname, './src/index.html'),
         filename: 'index.html',
         inject: 'body'
       }),
-
       isDev(new webpack.HotModuleReplacementPlugin()),
-
-      // Only running UglifyJsPlugin() in production
       ifProd(
         new webpack.optimize.UglifyJsPlugin({
           compress: {
             screw_ie8: true,
             warnings: false,
-            unused: true,
-            dead_code: true
+            unused: false,
+            dead_code: false
           },
           output: {
             comments: false
@@ -91,15 +73,13 @@ module.exports = env => {
           sourceMap: false
         })
       ),
-
       extractSass
     ]),
-
     devServer: {
       contentBase: srcDir,
       historyApiFallback: true,
       disableHostCheck: true,
-      hot: true,
+      hot: false,
       inline: true,
       port: 3000
     }
